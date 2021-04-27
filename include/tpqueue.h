3f89059a7f84f1f3591d3ba70e85e8fc2ab25aae
anton <ashtanyuk@gmail.com>
2021-04-27 15:33:43 +0300

    Исправления для cpplint

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,14 +1,16 @@
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
 #include <cassert>
- 
+
 template<typename T>
-class TPQueue
-{
+class TPQueue {
   // Сюда помещается описание структуры "Очередь с приоритетами"
 };
 
-
-struct SYM
-{
-	char ch;
-	int  prior;
+struct SYM {
+  char ch;
+  int  prior;
 };
+
+#endif // INCLUDE_TPQUEUE_H_
